515137402e38b62f4c5a361c31aa3b48365b1c37
christoffer.fjellstrom <christoffer.fjellstrom@qlok.se>
2023-11-08 11:50:37 +0100

    Add project files.

---
+++ BankBootstrap/Data/BankContext.cs
@@ -0,0 +1,22 @@
+﻿using BankBootstrap.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankBootstrap.Data
+{
+    internal class BankContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Fill in connection string here!!
+            optionsBuilder.UseSqlServer();
+        }
+    }
+}
